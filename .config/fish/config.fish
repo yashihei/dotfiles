5d8a50c83d8a79e07a23bcde9a1bc7829f8d2efe
yashihei <yashihei@gmail.com>
2019-04-13 18:30:21 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,16 @@
-rbenv init - | source
-fish_vi_key_bindings
+# rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+if which rbenv > /dev/null
+    rbenv init - | source
+end
+
+# vi keybind
+function fish_user_key_bindings
+  for mode in insert default visual
+    fish_default_key_bindings -M $mode
+  end
+  fish_vi_key_bindings --no-erase
+end
+
+# peco
+bind \cr 'peco_select_history (commandline -b)'
