f54c873db8afcdeaf9bfc1ab1d6f3228babb7fa8
yashihei <yashihei@gmail.com>
2019-04-13 16:11:39 +0900

    .configに対応

--- link.sh
+++ link.sh
@@ -3,16 +3,22 @@
 set -eu
 
 DOT_DIRECTORY="${HOME}/.dotfiles"
-# DOT_CONFIG_DIRECTORY=".config"
+DOT_CONFIG_DIRECTORY=".config"
 
 cd ${DOT_DIRECTORY}
 
 for f in .??*
 do
   # ignore files
-  [[ ${f} = ".git" ]] && continue
-  [[ ${f} = ".gitignore" ]] && continue
+  [ ${f} = ".git" ] && continue
+  [ ${f} = ".gitignore" ] && continue
+  [ ${f} = ${DOT_CONFIG_DIRECTORY} ] && continue
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+
+cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
+for file in `\find . -maxdepth 8 -type f`; do
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
 done
 
 echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
