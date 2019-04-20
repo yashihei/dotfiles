528c9d71baaee07818b77fed7941d3ff4336756c
yashihei <yashihei@gmail.com>
2019-04-21 01:52:36 +0900

    mklink

---
+++ mklink.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eu
+
+mklink() {
+  cmd.exe /c "mklink /D ..\${$1} ${$2}"
+}
+
+mklink .bash_aliases
+mklink .bash_profile
+mklink .bashrc
+mklink .bashrc_win
+mklink .gitconfig
+mklink .ideavimrc
+mklink .irbrc
+mklink .pryrc
+mklink .vimrc
