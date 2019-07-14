c50568fa9075daf5b5df4244b5c1a8664fe92a8a
yashihei <yashihei@gmail.com>
2019-07-14 17:47:14 +0900

    viモード、キーバインドがバッティングするのつらいのでオミット

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,12 @@ if which rbenv > /dev/null
 end
 
 # vi keybind
-function fish_user_key_bindings
-  for mode in insert default visual
-    fish_default_key_bindings -M $mode
-  end
-  fish_vi_key_bindings --no-erase
-end
+# function fish_user_key_bindings
+#   for mode in insert default visual
+#     fish_default_key_bindings -M $mode
+#   end
+#   fish_vi_key_bindings --no-erase
+# end
 
 # peco
 bind \cr 'peco_select_history (commandline -b)'
