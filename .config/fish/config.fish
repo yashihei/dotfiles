e8d489856f1bc31468cecee1bcd04fbd8efe8c9b
yashihei <yashihei@gmail.com>
2019-12-05 15:27:50 +0900

    Remove vi comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,14 +15,6 @@ set -g theme_display_k8s_context yes
 # benri commands
 set -x PATH $HOME/exec $PATH
 
-# vi keybind
-# function fish_user_key_bindings
-#   for mode in insert default visual
-#     fish_default_key_bindings -M $mode
-#   end
-#   fish_vi_key_bindings --no-erase
-# end
-
 # peco
 bind \cr 'peco_select_history (commandline -b)'
 
